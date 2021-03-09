ae9a92b66dd373a86444c2152167101296b83d74
Matheus Mortari <bmortari2000@gmail.com>
2021-03-09 20:05:33 -0300

    adding internet connection verification

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const puppeteer = require('puppeteer');
 const nodemailer = require('nodemailer');
 const { writeFileSync } = require('fs');
-const { cleanMoneyMask, dinheiroMask } = require('masks-br');
+const { cleanMoneyMask, dinheiroMask, porcentagemMask } = require('masks-br');
 const { CronJob } = require('cron');
+const https = require('https');
 
 const config = require('./config.json');
 const env = require('./env.json');
 
 const { products } = config;
 
-async function updateProductPrice(id, price) {
-  const to_update = [...products];
+async function updateProductPrice(index, price) {
+  const new_products = [...products];
 
-  const new_prods = to_update.map((product) => {
-    if (product.id === id) {
-      product.latestPrice = Number(price);
-    }
-
-    return product;
-  });
+  new_products[index].latestPrice = Number(price);
 
   const file = {
     ...config,
-    products: new_prods,
+    products: new_products,
   };
 
   const final_file = JSON.stringify(file, null, 2);
@@ -31,8 +26,6 @@ async function updateProductPrice(id, price) {
 }
 
 async function sendMail(productName, content) {
-  console.log('Enviando um email', productName);
-
   const transporter = nodemailer.createTransport({
     host: env.email.host,
     port: env.email.port,
@@ -52,15 +45,41 @@ async function sendMail(productName, content) {
   });
 }
 
+function calculatePercent(lastPrice, newPrice) {
+  return Number((100 - (100 * newPrice) / lastPrice).toFixed(2));
+}
+
+/**
+ * @returns {Promise<boolean>} Connection verification
+ */
+function checkInternetConnection() {
+  return new Promise((res) => {
+    https.get('https://www.amazon.com.br/', function (response) {
+      if (response.statusCode !== 200) {
+        res(false);
+      }
+
+      res(true);
+    });
+  });
+}
+
 async function fetchProducts() {
   console.log();
   console.log('Iniciando Script');
+
+  const check_connection = await checkInternetConnection();
+
+  if (!check_connection) {
+    console.log('Stopping script, no internet connection!');
+    return;
+  }
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await products.reduce(
-    (promise, product) =>
+    (promise, product, index) =>
       promise.then(async () => {
         console.log(`\n----------------- ${new Date()} -----------------`);
         console.log(`Buscando informações do produto - ${product.name} \n`);
@@ -72,23 +91,27 @@ async function fetchProducts() {
         let mailContent = '';
 
         if (productCurrentPrice <= product.intendedPrice) {
-          console.log(`Produto chegou ao preço que vc quer ${dinheiroMask(productCurrentPrice)}`);
+          console.log(`Produto chegou ao preço que você quer, ${dinheiroMask(productCurrentPrice)}`);
 
           mailContent += `Produto chegou ao preço que você quer, ${dinheiroMask(productCurrentPrice)}<br/><br/>`;
         }
 
         if (productCurrentPrice < product.latestPrice) {
+          const newPricePercent = calculatePercent(product.latestPrice, productCurrentPrice);
+
           console.log(
-            `Produto abaixou de preço, antes estava em ${dinheiroMask(product.latestPrice)}, agora está em ${dinheiroMask(productCurrentPrice)}`
+            `Produto abaixou de preço, antes estava em ${dinheiroMask(product.latestPrice)}, agora está em ${dinheiroMask(
+              productCurrentPrice
+            )}. Total de ${porcentagemMask(newPricePercent)} de desconto!`
           );
 
           mailContent += `Produto abaixou de preço, antes estava em ${dinheiroMask(product.latestPrice)}, agora está em ${dinheiroMask(
             productCurrentPrice
-          )}<br/><br/>`;
+          )}. Total de ${porcentagemMask(newPricePercent)} de desconto!<br/><br/>`;
         }
 
         if (productCurrentPrice !== product.latestPrice) {
-          updateProductPrice(product.id, productCurrentPrice);
+          updateProductPrice(index, productCurrentPrice);
         }
 
         if (mailContent !== '') {
